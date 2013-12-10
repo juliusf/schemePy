28ba8b4db9ddd8e92dc8383200fccdf4f4f38680
Julius F <julius.flohr@gmail.com>
2013-12-10 20:02:15 +0100

    fixed formatting

--- tests/scheme_tests.py
+++ tests/scheme_tests.py
@@ -2,36 +2,36 @@ from nose.tools import *
 from schemepy.scheme import *
 
 def test_SchemeProcedure():
-	proc = SchemeProcedure('test_procedure', ['+', 1, 2])
-	assert_equal(proc.name, 'test_procedure')
-	assert_equal(proc.impl, ['+', 1, 2])
-	assert_equal(proc.type, "Procedure")
-	assert_equal(proc.to_string(), "<Procedure:test_procedure>")
+    proc = SchemeProcedure('test_procedure', ['+', 1, 2])
+    assert_equal(proc.name, 'test_procedure')
+    assert_equal(proc.impl, ['+', 1, 2])
+    assert_equal(proc.type, "Procedure")
+    assert_equal(proc.to_string(), "<Procedure:test_procedure>")
 
 def test_SchemeString():
-	string = SchemeString('test_string')
-	assert_equal(string.value, 'test_string')
-	assert_equal(string.type, "String")
-	assert_equal(string.to_string(), '"test_string"')
+    string = SchemeString('test_string')
+    assert_equal(string.value, 'test_string')
+    assert_equal(string.type, "String")
+    assert_equal(string.to_string(), '"test_string"')
 
 def test_SchemeNumber():
-	number = SchemeNumber(3)
-	assert_equal(number.value, 3)
-	assert_equal(number.type, "Number")
-	assert_equal(number.to_string(), '3')
+    number = SchemeNumber(3)
+    assert_equal(number.value, 3)
+    assert_equal(number.type, "Number")
+    assert_equal(number.to_string(), '3')
 
 def test_environment_find():
-	env = environment(['a'], [1])
-	assert_equal(1, env.find('a'))
+    env = environment(['a'], [1])
+    assert_equal(1, env.find('a'))
 
 def test_environment_find_complex():
-	parent_env = environment(['a'],[1])
-	env = environment(['b'], [2], parent_env)
-	assert_equal(1, env.find('a'))
-	assert_raises(Exception, env.find, 'c')
+    parent_env = environment(['a'],[1])
+    env = environment(['b'], [2], parent_env)
+    assert_equal(1, env.find('a'))
+    assert_raises(Exception, env.find, 'c')
 
 def test_environment_update():
-	root_env = environment(['a'],[1])
-	dict = {'a':2}
-	root_env.update(dict)
-	assert_equal(root_env.find('a'),2)
+    root_env = environment(['a'],[1])
+    dict = {'a':2}
+    root_env.update(dict)
+    assert_equal(root_env.find('a'),2)
